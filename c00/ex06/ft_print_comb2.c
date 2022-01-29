70509a24080976208e87d7584488d32270fdf0d5
firoosou <raoufbouricha@gmail.com>
2022-01-29 01:11:04 +0100

    khedma

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <stdio.h>
+void ft_print_comb2();
+void ft_putchar(char c);
+
+int main ()
+{   ft_putchar('F');
+     ft_print_comb2();  
+    
+
+}
+
+
+void ft_putchar(char c)
+{
+    write(1,&c,1);
+}
+
+void ft_print_comb2()
+{
+int a;
+int b;
+a=0;
+
+    while (a<99)
+    {
+     //ft_putchar(a/10+'0');
+     //ft_putchar(a%10+'0');
+    // ft_putchar(' ');
+     b=a+1;
+         while(b<=99)
+         {
+         ft_putchar(a/10+'0');
+         ft_putchar(a%10+'0');
+         ft_putchar(' ');
+
+         ft_putchar(b/10+'0');
+         ft_putchar(b%10+'0');
+         if(a!=98 || b!=99)
+         {
+         ft_putchar(',');
+         }
+         b++;
+         }
+         
+     a++;
+
+
+    }
+
+
+
+}
+
+
+
+
