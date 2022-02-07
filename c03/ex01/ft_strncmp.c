4b8c6bdd187232c437f9a49bbb3c1ad59f6aabf5
firoosou <raoufbouricha@gmail.com>
2022-02-07 21:58:00 +0100

    upadeted pour c03

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,18 @@
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+int i;
+i = 0;
+while (s1[i])
+{
+while (i<n)
+{
+if (s1[i]!=s2[i])
+{
+return (s1[i] - s2[i]) ;
+}
+i++;
+}
+return (s1[i] - s2[i]);
+}
+return (s1[i] - s2[i]);
+}
